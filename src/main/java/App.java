580788ff6224a09a0c0edcb8344df30d03d6bf56
Matthieu Joannon <mattjoannon@gmail.com>
2019-01-16 16:45:48 +0100

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         GetFromApi.fetchAndSaveAllDepartements();
-        System.out.println("Done");
+        System.out.println("All good");
     }
 
 }
